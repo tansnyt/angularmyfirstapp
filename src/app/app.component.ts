250f83889e064ad834d4299842b7ccb0cd6b2405
tansnyt <taniasunyoto22@gmail.com>
2021-02-23 17:46:14 +0700

    angular tutorial finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+
+  a = 0;
+  b = 3;
+
+  item = {
+    nama = "Jeruk",
+    harga = 15000,
+    satuan = "kg"
+  };
+
+  items = [
+    { nama = "Jeruk", harga = 15000, satuan = "kg" },
+    { nama = "Apel", harga = 5000, satuan = "kg" }
+  ];
+
+  tampil: boolean = false;
+  tulisanOPEN: String;
+
+  OPEN() {
+    this.tulisanOPEN = "toko buka";
+  }
+
+  values = "";
+  onKey(event) {
+    this.values += event.target.value + '|';
+  }
 }
